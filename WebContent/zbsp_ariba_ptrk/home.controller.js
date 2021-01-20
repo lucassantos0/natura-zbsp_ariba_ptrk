8167c2b0c04384c4c0bfda2cd8bf1efc6f6bdf81
lucassantos0 <lubs123@gmail.com>
2021-01-20 09:57:02 -0300

    fix isNaN for datepicker

--- WebContent/zbsp_ariba_ptrk/home.controller.js
+++ WebContent/zbsp_ariba_ptrk/home.controller.js
@@ -10,9 +10,7 @@ sap.ui.controller("zbsp_ariba_ptrk.home", {
     "mainUrl" : "https://aslx62br.natura.com.br:4300/sap/bc/zariba/main/?sap-client=210&sap-user=GBLANCO&sap-password=Padawan2021",
     "mainCalculatedUrl" : "",
     "filter" : "",
-    "errorOnly" : true,
-    "dateFrom": "",
-    "dateTo" : ""
+    "errorOnly" : true
   },
 
   _oSupplierDashboardConfig: {
@@ -71,8 +69,8 @@ sap.ui.controller("zbsp_ariba_ptrk.home", {
     this._oConfig.mainCalculatedUrl = this._oConfig.mainUrl 
     	+ "&filter=" + this._oConfig.filter 
     	+ "&errorOnly=" + ( this._oConfig.errorOnly ? "true" : "false" )
-    	+ "&dateFrom=" + this._oQuoteRequestModel.dateFrom
-    	+ "&dateTo=" + this._oQuoteRequestModel.dateTo;
+    	+ ( !isNaN(this._oQuoteRequestModel.dateFrom) ? "&dateFrom=" + this._oQuoteRequestModel.dateFrom : "" )
+    	+ ( !isNaN(this._oQuoteRequestModel.dateTo) ? "&dateTo=" + this._oQuoteRequestModel.dateTo : "" )
     this._oQuoteRequestModel.loadData(this._oConfig.mainCalculatedUrl);
     this._oConfigModel.updateBindings();
     this._oSupplierDashboardModel.loadData(this._oSupplierDashboardConfig.url);
